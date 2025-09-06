f0e10bcae646128a226d0c73d78c483dbe047fc1
gh-actions <gh-actions>
2025-09-06 02:18:26 +0000

    autogen: release

--- types/adapter/entry-points/functions/index.d.ts
+++ types/adapter/entry-points/functions/index.d.ts
@@ -1,6 +1,2 @@
-export declare const functionGetCookieContent: (
-  username: string,
-  password: string,
-  authenticatorKey: string,
-) => Promise<string>;
+export declare const functionGetCookieContent: (username: string, password: string, authenticatorKey: string) => Promise<string>;
 //# sourceMappingURL=index.d.ts.map
