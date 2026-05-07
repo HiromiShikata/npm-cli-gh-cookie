982e79b433ea932b2cf8f222f8bc53a4c623f875
gh-actions <gh-actions>
2026-05-07 02:44:28 +0000

    autogen: release

--- types/domain/usecases/adapter-interfaces/GithubRepository.d.ts
+++ types/domain/usecases/adapter-interfaces/GithubRepository.d.ts
@@ -1,8 +1,4 @@
 export interface GithubRepository {
-  getCookieContent(
-    userName: string,
-    password: string,
-    authenticatorKey: string,
-  ): Promise<string>;
+    getCookieContent(userName: string, password: string, authenticatorKey: string): Promise<string>;
 }
 //# sourceMappingURL=GithubRepository.d.ts.map
